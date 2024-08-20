2c4017512564e24e570542db79abc6ee1f6e939e
Jasser Omri <jasser.omri@octo.com>
2024-08-20 21:56:35 +0200

    init

---
+++ lua/community.lua
@@ -0,0 +1,17 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.colorscheme.vscode-nvim"},
+  { import = "astrocommunity.recipes.vscode-icons"}
+  -- import/override with your plugins folder
+}
